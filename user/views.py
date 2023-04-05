f5880564ee8ce1cd785e5379b720304235a0b5d1
Muhammad Usama <81771131+muhammadusama6@users.noreply.github.com>
2023-04-05 15:40:32 +0500

    Fourth commit

--- user/views.py
+++ user/views.py
@@ -1,10 +1,8 @@
-from django.contrib.auth import authenticate
+from django.contrib.auth import authenticate, logout
 from django.contrib.auth.models import User
 from rest_framework import viewsets, status
 from rest_framework.response import Response
 from .serializers import UserSerializer
-
-
 class UserViewSet(viewsets.ModelViewSet):
     lookup_field = 'username'
     model = User
@@ -41,3 +39,7 @@ class UserViewSet(viewsets.ModelViewSet):
         else:
             return Response(data={'message': 'Invalid credentials'},
                             status=status.HTTP_401_UNAUTHORIZED)
+
+    def logout(self, request):
+        logout(request)
+        return Response({'message': 'Logged out successfully'})
